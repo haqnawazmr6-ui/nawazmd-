d6885217fc256eee76f41434b64a93ed20d6b52c
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-05-05 12:53:59 +0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *𝐍𝆭𝛂𝆭Ꮿ𝆭𝛂ʑ̽-𝐌𝛛͎free bot SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> N̥ͦḀͦW̥ͦḀͦZ̥ͦ-🄼🄳 Տᑭᗴᗴᗪ ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
